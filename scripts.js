eb160561aff2f8766375c5e64f56f5ebbe00719a
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2023-09-06 18:41:39 -0300

    Finalizar a base para os exercicios

---
+++ scripts.js
@@ -0,0 +1,8 @@
+console.log('Salveeee Baker')
+
+//-------------- RELEMBRANDO ---------
+//-------------- CONCEITOS. OBJETOS ---------
+//-------------- EXERCICIO 1 ---------
+//-------------- EXERCICIO 2 ---------
+//-------------- EXERCICIO 3 ---------
+//-------------- EXERCICIO EXTRA ---------
